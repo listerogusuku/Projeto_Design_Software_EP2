24b680a7e425f937f356216f367d4d10c56962aa
Celina Vieira de Melo <celinavm@al.insper.edu.br>
2021-04-27 11:10:45 -0300

    Criando baralho

--- paciencia.py
+++ paciencia.py
@@ -1,38 +1,40 @@
-# EP | Exercício Programa: Bacará Simplificado | Design de Software
-# Equipe: Celina Vieira de Melo e Lister Ogusuku Ribeiro
-# Data: 18/10/2020
-
-#Descrição:
-#Jogo de bacará criado com a intenção de promover uma interatividade com o usuário,
-#onde este aposta em quem será o vencedor da partida (jogador, banco ou empate) e, por fim, todo
-#o restante do jogo é realizado pela mesa de acordo com as regras simplificadas definidas ao longo
-#do programa pelo programador.
-
-#Neste jogo foram implementadas todas as regras avançadas.
-
-#============================
-
-#Importação da biblioteca "Random", que ajudará meu código a gerar números aleatórios
-
-
-print('\nPaciência Acordeão\n==================\n\nSeja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.\n\nExistem apenas dois movimentos possíveis:\n\n1. Empilhar uma carta sobre a carta imediatamente anterior;\n2. Empilhar uma carta sobre a terceira carta anterior.\n\nPara que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:\n\n1. As duas cartas possuem o mesmo valor ou \n2. As duas cartas possuem o mesmo naipe.\nDesde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.\n')
-
-start = int(input('Digite 1 para iniciar o jogo: '))
-
 import random
+def cria_baralho():
+    baralho= []
+    numeros = ['A', '2', '3','4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] 
+    naipe=['♠','♥','♦','♣']
+    
+    # num_total_cartas = 52
+    carta_espada= []
+    carta_copa = []
+    carta_ouros = []
+    carta_paus = []
+    i = 0
+    while i < len(numeros) :
+        nova1 = numeros[i] + naipe[0]
+        carta_espada.append(nova1)
+        nova2 = numeros[i] + naipe[1]
+        carta_copa.append(nova2)
+        nova3 = numeros[i] + naipe[2]
+        carta_ouros.append(nova3)
+        nova4 = numeros[i] + naipe[3]
+        carta_paus.append(nova4)
+        i += 1
+    baralho = carta_espada + carta_copa + carta_paus + carta_ouros
+    random.shuffle(baralho)
+    a = 0
+    while a < len(baralho):
+        print('{0}. {1}'.format(a+1,baralho[a]))
+        a += 1
 
-baralho=['Ás', 2, 3, 4, 5, 6, 7, 8, 9, 10,'J', 'Q', 'K']  # Criação de uma lista com os índices do baralho.
-naipe=['Ouro','Paus','Copas','Espada']
+    carta = int(input('Escolha uma carta de 1 a 52: '))
+    while carta <= 0 or carta > 52:
+        print('Não entendi, digite um número entre o intervalo pedido ')
+    carta_no_baralho = baralho[carta-1]
+    print('Sua carta é: {}'.format(carta_no_baralho))
+    
+    return baralho
+
+cria_baralho()
 
 
-
-
-
-# Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: 
-
-# 1. As duas cartas possuem o mesmo valor ou 
-# 2. As duas cartas possuem o mesmo naipe. 
-
-# Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. 
-
-# Aperte [Enter] para iniciar o jogo...
--- teste.py
+++ teste.py
@@ -1,6 +1,120 @@
-print('just do it')
+# def extrai_naipe(string):
+#     print(string)
+#     if len(string) == 2:
+#         so_naipe = string[1]
+#     else:
+#         so_naipe = string[2]
+#     print(so_naipe)
+#     return so_naipe
 
-#test
+# def extrai_valor(string):
+#     print(string)
+#     if len(string) == 2:
+#         so_naipe = string[0]
+#     else:
+#         so_naipe = string[0] + string[1]
+#     print(so_naipe)
+#     return so_naipe
 
-#test 2
+# def possui_movimentos_possiveis(lista):
+#     if lista[0]:
+#         return False
+#     elif lista[2] == 1:
+#     if extrai_valor(lista[1]) == extrai_valor(lista[0]) or extrai_naipe(lista[1]) == extrai_naipe(lista[0]):
+#         return True
+#     else:
+#         return False
+#     elif i == 2:
+#         if extrai_valor(lista[2]) == extrai_valor(lista[1]) or extrai_naipe(lista[2]) == extrai_naipe(lista[1]):
+#             return True
+#         else:
+#             return False
+#     i=3
+#     while i < len(lista):
 
+#         if (extrai_valor(lista[i]) == extrai_valor(lista[i-1]) or extrai_naipe(lista[i]) == extrai_naipe(lista[i-1])) and ((extrai_valor(lista[i]) == extrai_valor(lista[i-3]) or extrai_naipe(lista[i]) == extrai_naipe(lista[i-3]))):
+#             return True
+#         elif (extrai_valor(lista[i]) == extrai_valor(lista[i-1]) or extrai_naipe(lista[i]) == extrai_naipe(lista[i-1])):
+#             return True
+#         elif (extrai_valor(lista[i]) == extrai_valor(lista[i-3]) or extrai_naipe(lista[i]) == extrai_naipe(lista[i-3])):
+#             return True
+#         else:
+#             return False
+#         i+=1
+def cria_baralho():
+    baralho = []
+    numeros = ['A', '2', '3','4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] 
+    naipe=['♠','♥','♦','♣']
+    
+    # num_total_cartas = 52
+    carta_espada= []
+    carta_copa = []
+    carta_ouros = []
+    carta_paus = []
+    i = 0
+    while i < len(numeros) :
+        nova1 = numeros[i] + naipe[0]
+        carta_espada.append(nova1)
+        nova2 = numeros[i] + naipe[1]
+        carta_copa.append(nova2)
+        nova3 = numeros[i] + naipe[2]
+        carta_ouros.append(nova3)
+        nova4 = numeros[i] + naipe[3]
+        carta_paus.append(nova4)
+        i += 1
+    baralho = carta_espada + carta_copa + carta_paus + carta_ouros
+    return baralho    
+
+ 
+def extrai_naipe(string):
+    if len(string) == 2:
+        so_naipe = string[1]
+    else:
+        so_naipe = string[2]
+
+    return so_naipe
+
+def extrai_valor(string):
+
+    if len(string) == 2:
+        so_naipe = string[0]
+    else:
+        so_naipe = string[0] + string[1]
+
+    return so_naipe
+
+def lista_movimentos_possiveis(baralho, posicao):
+    if posicao == 0:
+        return[]
+    elif posicao == 1:
+        if extrai_valor(baralho[1]) == extrai_valor(baralho[0]) or extrai_naipe(baralho[1]) == extrai_naipe(baralho[0]):
+            return[1]
+        else:
+            return[]
+    elif posicao == 2:
+        if extrai_valor(baralho[2]) == extrai_valor(baralho[1]) or extrai_naipe(baralho[2]) == extrai_naipe(baralho[1]):
+            return[1]
+        else:
+            return[] 
+    elif (extrai_valor(baralho[posicao]) == extrai_valor(baralho[posicao-1]) or extrai_naipe(baralho[posicao]) == extrai_naipe(baralho[posicao-1])) and (extrai_valor(baralho[posicao]) == extrai_valor(baralho[posicao-3]) or extrai_naipe(baralho[posicao]) == extrai_naipe(baralho[posicao-3])):
+        return[1,3]
+    elif (extrai_valor(baralho[posicao]) == extrai_valor(baralho[posicao-1]) or extrai_naipe(baralho[posicao]) == extrai_naipe(baralho[posicao-1])):
+        return[1]
+    elif extrai_valor(baralho[posicao]) == extrai_valor(baralho[posicao-3]) or extrai_naipe(baralho[posicao]) == extrai_naipe(baralho[posicao-1]):
+        return[3]
+    else:
+        return[]
+
+def possui_movimentos_possiveis(lista):
+    i = 0
+    while i < len(lista):
+        if lista_movimentos_possiveis(lista,i) == [1,3] or lista_movimentos_possiveis(lista, i) == [1] or lista_movimentos_possiveis(lista, i) == [3]:
+            print(lista_movimentos_possiveis(x, i))
+            return True
+        else:
+            i+=1
+    return False
+
+lista = ['A♦', '10♥', 'Q♣', '4♠']
+print(possui_movimentos_possiveis(lista))
+            

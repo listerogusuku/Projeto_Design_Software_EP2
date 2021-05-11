3c11e50e158bc96ac3908223a5957398a3bdf6bc
Lister Ogusuku Ribeiro <listeror@al.insper.edu.br>
2021-05-10 23:12:56 -0300

    edicao_do_cabecalho

--- paciencia.py
+++ paciencia.py
@@ -1,7 +1,10 @@
 # EP | Exercício Programa: Paciência Acordeão | Design de Software
+# Professor: Humberto Sandmann
 # Equipe: Celina Vieira de Melo e Lister Ogusuku Ribeiro
 # Data: 01/05/2021
 
+#No código a seguir foi feita a implementação do jogo "Paciência Acordeão" (Solitaire Accordion).
+#O jogo faz parte do "Exercício Programa 2" da Disciplina Design de Software.
 
 #Neste jogo foram implementadas regras avançadas.
 
